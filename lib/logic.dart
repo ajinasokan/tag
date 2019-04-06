12598bfb6b628e41b99c468a8f70fc12104bd68f
Ajin Asokan <ajin.panthayil@gmail.com>
2019-04-06 22:08:46 +0530

    less than 5120

--- lib/logic.dart
+++ lib/logic.dart
@@ -7,15 +7,12 @@ import 'package:sensors/sensors.dart';
 // State
 
 class Player {
-  int x = 0, y = 0, tag = 0, score = 5;
+  var b = [0, 0, 0, 5];
+  get x => b[0];
+  get y => b[1];
+  get tag => b[2];
+  get score => b[3];
   InternetAddress ip = InternetAddress.loopbackIPv4;
-  get data => [x, y, tag, score];
-  set data(List<int> bin) {
-    x = bin[0];
-    y = bin[1];
-    tag = bin[2];
-    score = bin[3];
-  }
 }
 
 int xMax = 143;
@@ -25,7 +22,7 @@ Player lastTag;
 String message;
 List<Player> players = [];
 
-player(int id) {
+Player player(int id) {
   for (var i = players.length; i <= id; i++) players.add(Player());
   return players[id];
 }
@@ -50,41 +47,28 @@ onData(_) {
     if (id == -1) id = players.length;
     udp.send([1, id], ip, 54321);
     if (id < 6) player(id).ip = ip;
-    updater.add(null);
   } else if (bin[0] == 1) {
     // join response: 1, id
     myID = bin[1];
     udp.send([2, myID, 127, 127], ip, 54321);
-    updater.add(null);
   } else if (bin[0] == 2) {
     // delta: 2, id, dx, dy
     process(bin[1], bin[2] - 127, bin[3] - 127);
   } else if (bin[0] == 3) {
     // position: 3, x, y, tag, score
-    for (var i = 1, id = (i - 1) ~/ 4; i < bin.length; i += 4) {
-      // print(bin.sublist(i));
-      player(id).data = bin.sublist(i);
-    }
-
-    update();
+    for (var i = 1, id = (i - 1) ~/ 4; i < bin.length; i += 4)
+      player(id).b = bin.sublist(i);
   }
+
+  update();
 }
 
-// int lastEvent = 0;
 initSensors() async {
   accelerometerEvents.listen((e) async {
-    // var now = DateTime.now().millisecondsSinceEpoch;
     if (myID == null) return;
-    // if (myID == null || now - lastEvent < 200) return;
-
     var x = e.x.toInt(), y = e.y.toInt();
     if (x == 0 && y == 0) return;
-    // lastEvent = now;
-    // var send = () async {
     udp.send([2, myID, x + 127, y + 127], await serverIP, 54321);
-    // };
-    // send();
-    // Future.delayed(Duration(milliseconds: 100), () => send());
   });
 }
 
@@ -110,45 +94,41 @@ create() async {
     return;
   }
   myID = 0;
-  player(0).tag = 1;
+  player(0).b[2] = 1;
   update();
 }
 
 get serverIP async => InternetAddress("192.168.43.209");
 
-join() async {
-  udp.send([0], await serverIP, 54321);
-}
+join() async => udp.send([0], await serverIP, 54321);
 
-distance(Player a, Player b) {
-  return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
-}
+dist(Player a, Player b) => sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 
 process(int id, int dx, int dy) {
   // print("process $id, $dx, $dy");
   if (player(id).score == 0) return;
 
-  player(id).x = (player(id).x - dx).clamp(0, xMax);
-  player(id).y = (player(id).y + dy).clamp(0, yMax);
+  player(id).b[0] = (player(id).x - dx).clamp(0, xMax);
+  player(id).b[1] = (player(id).y + dy).clamp(0, yMax);
 
   var tag = players.firstWhere((p) => p.tag == 1);
 
   for (var i = 0; i < players.length; i++) {
     var p = player(i);
-    if (p != lastTag && p != tag && p.score > 0 && distance(tag, p) < 15) {
-      p.score--;
+    if (p != lastTag && p != tag && p.score > 0 && dist(tag, p) < 15) {
+      p.b[3]--;
       // lastTag = tag;
 
       if (p.score != 0) {
-        tag.tag = 0;
-        p.tag = 1;
+        tag.b[2] = 0;
+        p.b[2] = 1;
       }
       break;
     }
   }
 
   List<int> data = [3];
-  players.forEach((p) => data.addAll(p.data));
+  players.forEach((p) => data.addAll(p.b));
 
   players.forEach((p) => udp.send(data, p.ip, 54321));
   update();
--- lib/main.dart
+++ lib/main.dart
@@ -13,52 +13,32 @@ void main() {
   runApp(app());
 }
 
-class Dots extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    players.forEach((d) {
-      if (d.score <= 0) return;
-      var p = Paint()..color = Colors.white;
-      var o = Offset(size.width * d.x / xMax, size.height * d.y / yMax);
-      canvas.drawCircle(o, 16, p);
-      p.color = Colors.black;
-      if (d.tag == 1) canvas.drawCircle(o, 8, p);
-    });
-  }
-
-  @override
-  bool shouldRepaint(_) => true;
-}
-
 scoreItem(Player data) {
   return Row(
-    mainAxisSize: MainAxisSize.min,
     children: <Widget>[
-      Container(
-        height: 16,
-        width: 16,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          shape: BoxShape.circle,
-        ),
-      ),
+      Icon(Icons.lens, size: 16),
       Text(" " + data.score.toString(), style: smallText)
     ],
   );
 }
 
-button(String title, VoidCallback onTap) {
-  return FlatButton(
-    onPressed: onTap,
-    child: Text(title, style: smallText),
+playerItem(Player d, BoxConstraints con) {
+  return Positioned(
+    left: con.maxWidth * d.x / xMax - 16,
+    top: con.maxHeight * d.y / yMax - 16,
+    child: Icon(Icons.lens, size: 32),
   );
 }
 
+button(String title, VoidCallback onTap) {
+  return FlatButton(onPressed: onTap, child: Text(title, style: smallText));
+}
+
 startMenu() {
+  if (myID != null) return Container();
   return Center(
     child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
       children: <Widget>[
         message != null ? Text(message, style: smallText) : Container(),
         button("Host game", create),
@@ -74,17 +54,16 @@ home() {
     bottomNavigationBar: Container(
       color: Colors.black,
       padding: EdgeInsets.all(16),
-      child: Wrap(
-        direction: Axis.horizontal,
-        spacing: 32,
-        runSpacing: 8,
-        children: [Text("Score")]..addAll(players.map((p) => scoreItem(p))),
+      child: Row(
+        children: [Text("Score   ")]..addAll(players.map((p) => scoreItem(p))),
       ),
     ),
-    body: CustomPaint(
-      painter: Dots(),
-      child: myID == null ? startMenu() : Container(),
-    ),
+    body: LayoutBuilder(builder: (_, con) {
+      return Stack(
+        children: [Positioned.fill(child: startMenu())]
+          ..addAll(players.map((p) => playerItem(p, con))),
+      );
+    }),
   );
 }
 
